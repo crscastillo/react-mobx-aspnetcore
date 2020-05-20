e74247f92ba36f9f9c0acda299cfab9695191c42
crscastillo <crscastillo@gmail.com>
2020-05-20 17:00:03 -0600

    installed mobx and simple use in our app

--- reactivities/client-app/src/app/layout/App.tsx
+++ reactivities/client-app/src/app/layout/App.tsx
@@ -1,12 +1,15 @@
-import React, { useState, useEffect, Fragment, SyntheticEvent } from "react";
+import React, { useState, useEffect, Fragment, SyntheticEvent, useContext } from "react";
 import { Container } from "semantic-ui-react";
 import { IActivity } from "../models/activity";
 import ActivityDashboard from "../../features/activities/dashboard/ActivityDashboard";
 import { NavBar } from "../../features/nav/NavBar";
 import agent from "../api/agent";
 import LoadingComponent from "./LoadingComponent";
+import ActivityStore from "../stores/activityStore";
 
 const App = () => {
+  const activityStore = useContext(ActivityStore);
+
   const [activities, setActivities] = useState<IActivity[]>([]);
 
   const [selectedActivity, setSelectedActivity] = useState<IActivity | null>(null);
@@ -70,6 +73,7 @@ const App = () => {
     <Fragment>
       <NavBar openCreateForm={handleOpenCreateForm} />
       <Container style={{ marginTop: "7em" }}>
+        <h1>{activityStore.title}</h1>
         <ActivityDashboard
           activities={activities}
           selectActivity={handleSelectActivity}
