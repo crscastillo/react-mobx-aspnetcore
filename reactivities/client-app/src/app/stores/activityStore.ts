cc77fd0a00c0e7f3023300e5e88e597100ea60c2
crscastillo <crscastillo@gmail.com>
2020-05-29 16:40:50 -0600

    added error handling with react-toastify and axios

--- reactivities/client-app/src/app/stores/activityStore.ts
+++ reactivities/client-app/src/app/stores/activityStore.ts
@@ -37,8 +37,7 @@ class ActivityStore {
           this.activityRegistry.set(activity.id, activity);
         });
         this.loadingInitial = false;
-      })
-      console.log(this.groupActivitiesByDate(activities));
+      });      
     } catch (error) {
       runInAction('load activities error', () => {
         this.loadingInitial = false;
