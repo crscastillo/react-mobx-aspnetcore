3e059689d0344e633f3e61ff8f1525f7728cf094
crscastillo <crscastillo@gmail.com>
2020-06-10 07:37:47 -0600

    photo upload client work

--- client-app/src/app/stores/rootStore.ts
+++ client-app/src/app/stores/rootStore.ts
@@ -4,20 +4,23 @@ import { createContext } from "react";
 import { configure } from "mobx";
 import CommonStore from "./commonStore";
 import ModalStore from "./modalStore";
+import ProfileStore from "./profileStore";
 
 configure({ enforceActions: 'always' });
 
 export class RootStore {
-    activityStore: ActivityStore; 
+    activityStore: ActivityStore;
     userStore: UserStore;
     commonStore: CommonStore;
     modalStore: ModalStore;
+    profilestore: ProfileStore;
 
-    constructor(){
+    constructor() {
         this.activityStore = new ActivityStore(this);
         this.userStore = new UserStore(this);
         this.commonStore = new CommonStore(this);
         this.modalStore = new ModalStore(this);
+        this.profilestore = new ProfileStore(this);
     }
 }
 
---
+++ client-app/src/features/profiles/ProfilePage.tsx
@@ -0,0 +1,38 @@
+import React, { useContext, useEffect } from "react";
+import { Grid } from "semantic-ui-react";
+import ProfileHeader from "./ProfileHeader";
+import ProfileContent from "./ProfileContent";
+import { RootStoreContext } from "../../app/stores/rootStore";
+import { RouteComponentProps } from "react-router-dom";
+import LoadingComponent from "../../app/layout/LoadingComponent";
+import { observer } from "mobx-react-lite";
+
+interface RoutParams {
+  username: string;
+}
+
+interface IProps extends RouteComponentProps<RoutParams> {}
+
+const ProfilePage: React.FC<IProps> = ({ match }) => {
+  const rootStore = useContext(RootStoreContext);
+  const { loadingProfile, profile, loadProfile } = rootStore.profilestore;
+
+  useEffect(() => {
+    loadProfile(match.params.username);
+  }, [loadProfile, match]);
+
+  if (loadingProfile) {
+    return <LoadingComponent content="Loading profile ..." />;
+  }
+
+  return (
+    <Grid>
+      <Grid.Column width={16}>
+        <ProfileHeader profile={profile!} />
+        <ProfileContent />
+      </Grid.Column>
+    </Grid>
+  );
+};
+
+export default observer(ProfilePage);
